267ca910ed123a045c974b5791c67e3fc89f5437
stef <stef.styl8@gmail.com>
2022-02-02 14:50:59 +0200

    double guesses and input validation

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const resetButton = document.querySelector(".play-again hide");
 
 const word = "mangolia";
+const guessedLetters = [];
 
 const start = function(word){
     const wordInArray = [];
@@ -23,7 +24,23 @@ start(word);
 
 guessButton.addEventListener("click",function(e){
     e.preventDefault();
+    message.innerText = "";
     const letter = letterInput.value;
-    console.log(letter);
+    const validatedLetter = inputValidation(letter);
+    if(validatedLetter){makeGuess(validatedLetter);console.log(validatedLetter);}
     letterInput.value="";
 });
+
+const inputValidation = function (input){
+    const acceptedInput = /[a-zA-Z]/;
+    if (input.length===0){message.innerText = "Please enter a letter";}
+    else if (input.length>1){message.innerText="Please enter a single letter";}
+    else if (!input.match(acceptedInput)){message.innerText="Please a valid letter from A to Z";}
+    else{return input;}
+};
+
+const makeGuess = function (letter){
+    letter = letter.toUpperCase();
+    if (guessedLetters.includes(letter)){message.innerText="You already guessed that letter. Try again.";}
+    else {guessedLetters.push(letter);console.log(guessedLetters.join(","))}
+};
